b9e8c57a7b12807b9d1ad3c55a1078cb73f5c461
Sui / SuicideMachine <SuiMachine@users.noreply.github.com>
2025-08-16 17:52:24 +0200

    Fix incorrect request

--- API/Helix/Request/Request_ModifyChannelInformation.cs
+++ API/Helix/Request/Request_ModifyChannelInformation.cs
@@ -21,7 +21,7 @@
 		public string game_id;
 		public string broadcaster_language;
 		public string title;
-		public int? delay = 0;
+		public int? delay;
 		public string[] tags;
 		public ContentClassification[] content_classification_labels;
 		public bool? is_branded_content;
